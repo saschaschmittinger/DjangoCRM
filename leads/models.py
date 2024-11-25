72bcf713b3b6aebbcdf5ae469c4ef36fc77d5aa2
Sascha Schmittinger <saschatimoschmittinger@gmail.com>
2024-11-25 09:28:02 +0100

    25_11_24

--- leads/models.py
+++ leads/models.py
@@ -3,7 +3,6 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
 
 
-
 class User(AbstractUser):
     is_organisor = models.BooleanField(default=True)
     is_agent = models.BooleanField(default=False)
@@ -11,30 +10,32 @@ class User(AbstractUser):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return self.user.username
-
 
 
 class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
-    agent = models.ForeignKey("Agent",on_delete=models.CASCADE)   
-    
+    adress = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=50, blank=True, null=True)
+    post_code = models.CharField(max_length=50, blank=True, null=True)
+    phone = models.CharField(max_length=50, blank=True, null=True)
+    mobile_phone = models.CharField(max_length=50, blank=True, null=True)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
-        
-     
+
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return self.user.email
-
 
 
 def postUserCreatedSignal(sender, instance, created, **kwargs):
